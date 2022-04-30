6e2edee5695c15306696ff6fdda7fc370f9aff85
Francisco Javier Avilés <franciscojavieravifer@gmail.com>
2022-04-30 10:28:57 +0200

    First commit

--- app/src/main/java/es/avifer/pageindicatorapp/MainActivity.kt
+++ app/src/main/java/es/avifer/pageindicatorapp/MainActivity.kt
@@ -1,7 +1,7 @@
-package es.avifer.pageindicator
+package es.avifer.pageindicatorapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
